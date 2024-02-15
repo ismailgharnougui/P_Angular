1c3c63441be502a3571848620c1b51ea72d02630
Ismail Elgharnougui <72952720+ismailgharnougui@users.noreply.github.com>
2024-02-15 10:32:18 +0100

    commit

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent {
+login=new FormControl('',Validators.required);
+  pwd=new FormControl('',Validators.required);
+  save() {
+    console.log(this.login.value)
+    console.log(this.pwd.value)
 
+  }
 }
